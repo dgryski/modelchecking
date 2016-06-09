900d9b266efce5da86d1942aef54e90955271ad4
Damian Gryski <dgryski@gmail.com>
2016-06-09 08:41:59 +0200

    go/banana: more work to make it look like autogenerated output

--- go/banana/banana.go
+++ go/banana/banana.go
@@ -4,12 +4,53 @@ package main
 import (
 	"flag"
 	"log"
-	"math/rand"
 	"os"
 )
 
-var bananas = 2
-var notePresent bool
+type context struct {
+	clock int
+	rand  rng
+
+	env struct {
+		bananas     int
+		notePresent bool
+
+		roommates [5]Person
+	}
+}
+
+func newContext() *context {
+	var c context
+	c.rand = 0x123456789ABCDEF
+	c.env.bananas = 2
+	for i := range c.env.roommates {
+		c.env.roommates[i] = newPerson()
+	}
+
+	return &c
+}
+
+func (ctx *context) assertFailed(s string) {
+	log.Println("assertion failed: ", s)
+	ctx.log()
+	os.Exit(1)
+}
+
+func bananaRangeCheck(ctx *context) {
+	if ctx.env.bananas >= 0 && ctx.env.bananas <= 100 {
+		return
+	}
+	ctx.assertFailed("bananas out of range: 0..100")
+}
+
+func (ctx *context) log() {
+	log.Printf("state at time = %+v\n", ctx.clock)
+	log.Printf("bananas = %d\n", ctx.env.bananas)
+	log.Printf("notePresent = %+v\n", ctx.env.notePresent)
+	for i := range ctx.env.roommates {
+		log.Printf("env.roommates[%d] = %+v\n", i, ctx.env.roommates[i])
+	}
+}
 
 //go:generate stringer -type PersonState .
 
@@ -22,6 +63,11 @@ const (
 	PersonReturningFromStore
 )
 
+func newPerson() Person {
+	var p Person
+	return p
+}
+
 type Person struct {
 	State PersonState
 
@@ -31,70 +77,92 @@ type Person struct {
 	Carrying int
 }
 
-var roommates [5]Person
-
-func dumpState(steps int) {
-	log.Printf("state at time = %+v\n", steps)
-	log.Printf("bananas = %d\n", bananas)
-	log.Printf("notePresent = %+v\n", notePresent)
-	for i := range roommates {
-		log.Printf("roommates[%d] = %+v\n", i, roommates[i])
-	}
-}
-
-func invariantBananaLimit() {
-	if bananas <= 8 {
+func invariantBananaLimit(ctx *context) {
+	if ctx.env.bananas <= 8 {
 		return
 	}
 
-	dumpState(steps)
-	os.Exit(1)
+	ctx.assertFailed("bananas <= 8")
 }
 
-var steps int
+func ruleStep(ctx *context, i int) {
+	switch ctx.env.roommates[i].State {
+	case PersonHappy:
+		ctx.env.roommates[i].State = PersonHungry
+	case PersonHungry:
+		if ctx.env.bananas == 0 {
+			if !*broken && ctx.env.notePresent {
+				// roommate at store -- try again later
+			} else {
+				ctx.env.notePresent = true
+				ctx.env.roommates[i].State = PersonGoingToStore
+			}
+		} else {
+			ctx.env.bananas -= 1
+			bananaRangeCheck(ctx)
+			ctx.env.roommates[i].State = PersonHappy
+		}
+	case PersonGoingToStore:
+		ctx.env.roommates[i].State = PersonReturningFromStore
+		ctx.env.roommates[i].Carrying = int(ctx.rand.Intn(9)) // 0..8
+	case PersonReturningFromStore:
+		ctx.env.bananas += ctx.env.roommates[i].Carrying
+		bananaRangeCheck(ctx)
+		ctx.env.roommates[i].Carrying = 0
+		ctx.env.notePresent = false
+		ctx.env.roommates[i].State = PersonHungry
+	}
+}
+
+var broken = flag.Bool("broken", false, "run broken algorithm")
 
 func main() {
 	verbose := flag.Bool("v", false, "verbose")
-	broken := flag.Bool("broken", false, "run broken algorithm")
 
 	flag.Parse()
 
-	for {
-		i := rand.Intn(len(roommates)) // pick a person to move
+	ctx := newContext()
 
-		switch roommates[i].State {
-		case PersonHappy:
-			roommates[i].State = PersonHungry
-		case PersonHungry:
-			if bananas == 0 {
-				if !*broken && notePresent {
-					// roommate at store -- try again later
-				} else {
-					notePresent = true
-					roommates[i].State = PersonGoingToStore
-				}
-			} else {
-				bananas -= 1
-				roommates[i].State = PersonHappy
-			}
-		case PersonGoingToStore:
-			roommates[i].State = PersonReturningFromStore
-			roommates[i].Carrying = rand.Intn(9) // 0..8
-		case PersonReturningFromStore:
-			bananas += roommates[i].Carrying
-			roommates[i].Carrying = 0
-			notePresent = false
-			roommates[i].State = PersonHungry
-		}
-		invariantBananaLimit()
-		steps++
+	var rules []func(*context)
+
+	for i := range ctx.env.roommates {
+		i := i
+		rules = append(rules, func(ctx *context) { ruleStep(ctx, i) })
+	}
+
+	for {
+		r := rules[ctx.rand.Intn(uint64(len(rules)))]
+
+		r(ctx)
+
+		invariantBananaLimit(ctx)
 
 		if *verbose {
-			dumpState(steps)
+			ctx.log()
 		}
+		ctx.clock++
+	}
+}
 
-		if steps%102400 == 0 {
-			log.Printf("steps = %+v\n", steps)
+type rng uint64
+
+func (r *rng) next() uint64 {
+	x := *r
+	x ^= x >> 12 // a
+	x ^= x << 25 // b
+	x ^= x >> 27 // c
+	*r = x
+	return uint64(x * 2685821657736338717)
+
+}
+
+// Bound returns a uniform integer 0..n-1
+func (r *rng) Intn(n uint64) uint64 {
+	threshold := -n % n
+	for {
+		x := r.next()
+		if x >= threshold {
+			return x % n
 		}
 	}
 }
